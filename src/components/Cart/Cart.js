b097eec6803f336c090a94f5f290f6344be0332b
Ernesto Gilberto Valenzuela Gallardo <ernestogilberto@hotmail.com>
2021-12-02 14:57:10 -0600

    Creando ordenes y actualizando stock.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,14 +2,44 @@ import React, {useContext} from 'react';
 import {CartContext} from '../contexts/CartContext';
 import {Link} from 'react-router-dom';
 import './Cart.css';
+import {collection, doc, increment, setDoc, updateDoc} from 'firebase/firestore';
+import db from '../../utils/firebaseConfig';
 
 const Cart = () => {
 
   const context = useContext(CartContext);
-  const {deleteAll} = context;
-  const {deleteItem} = context;
-  const {totalItem} = context;
-  const {total} = context;
+  const {cartList, deleteAll, deleteItem, totalItem, total} = context;
+
+  const createOrder = () => {
+    const order = {
+      buyer: {
+        name: 'John Doe',
+        email: 'john@doe.com',
+        phone: '123456789'
+      },
+      items: cartList.map(item => ({
+        id: item.id,
+        title: item.name,
+        price: item.price
+      })),
+      total: total(),
+    };
+
+    (async function () {
+      const newOrderRef = doc(collection(db, 'orders'))
+      await setDoc(newOrderRef, order);
+      return newOrderRef
+    })()
+
+    cartList.forEach(async item => {
+      const ItemRef = doc(collection(db, 'beers'), item.id)
+      await updateDoc(ItemRef, {
+        stock: increment(-item.quantity)
+      })
+    })
+
+    deleteAll();
+  }
 
   return (
       <div className="cart-container">
@@ -19,7 +49,7 @@ const Cart = () => {
             <button className="continue-btn">Seguir Comprando</button>
           </Link>
         </div>
-        {context.cartList.length > 0 ? (context.cartList.map(item => (
+        {context.cartList.length > 0 ? (cartList.map(item => (
                 <div className="cart-item" key={item.id}>
                   <div>
                     <img src={item.img} alt={item.name}/>
@@ -34,7 +64,10 @@ const Cart = () => {
                 </div>))
         ) : <h1 className="empty-cart">No hay items en el carrito</h1>}
         <h1>Total: $ {total().toFixed(2)}</h1>
-        {context.cartList.length > 0 && <button className="deleteAll-btn" onClick={deleteAll}>Vaciar Carrito</button>}
+        {context.cartList.length > 0 && (<>
+          <button className="deleteAll-btn" onClick={deleteAll}>Vaciar Carrito</button>
+          <button className="create-order" onClick={createOrder}>CheckOut</button>
+        </>)}
       </div>
   );
 }
